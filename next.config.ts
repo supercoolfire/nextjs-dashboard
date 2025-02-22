29041162b23f934c2b03d0a7c5788cd1bcad8e29
supercoolfire <bahuiput@gmail.com>
2025-02-22 18:56:58 +0800

    Initial commit; duplicate upload

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    ppr: "incremental"
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
